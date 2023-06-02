f7d1a38c9fada039e41f6b098f45dc5fb24c9497
swethamahadeva <131241749+swethamahadeva@users.noreply.github.com>
2023-06-02 17:54:50 +0530

    Add files via upload

---
+++ Program206.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Program206
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int ascii_var=(int)ch;
+System.out.println("ASCII of"+ ch +"is"+ascii_var);
+}
+}
